9d112b248e74d368122548bbda7a31fb5c6be0ff
sujith kumar <148097955+sujith52@users.noreply.github.com>
2024-08-13 15:51:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # Novel page link
-novel_page_link = "https://example.com/novel-page"
+novel_page_link = "https://lightnovel.novelupdates.net/book/emperors-domination/chapter-1"
 
 # Send a GET request to the novel page
 response = requests.get(novel_page_link)
